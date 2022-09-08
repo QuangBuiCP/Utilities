dbf134fe250104713488413617ac12746297932a
QaergzBit <buithanhquang2006@gmail.com>
2022-09-08 18:37:50 +0700

    Add Sublime text config

---
+++ Path/7z.cmd
@@ -0,0 +1,3 @@
+@echo off
+
+"C:\Program Files\7-Zip\7z.exe" %*
--- Templates/cpp/Saved/segtree-it.cpp
+++ Templates/cpp/Saved/segtree-it.cpp
@@ -1,41 +1,41 @@
-template <typename T, class F = function<T(const T&, const T&)>>
+template <typename T, class F = std::function<T(const T&, const T&)>>
 struct SegmentTree {
-  vector<T> seg;
-  T unit;
-  int N;
-  F func;
+	std::vector<T> tree;
+	T unit;
+	int N;
+	F func;
 
-  SegmentTree(vector<int> a, T def, F f) : seg(2 * int(a.size()) + 1, def), unit(def), N(int(a.size())), func(f) { build(a); }
-  SegmentTree(int n, T def, F f) : seg(2 * n + 1, def), unit(def), N(n), func(f) {}
+	SegmentTree(std::vector<int> a, T def, F f) : tree(2 * int(a.size()) + 1, def), unit(def), N(int(a.size())), func(f) { build(a); }
+	SegmentTree(int n, T def, F f) : tree(2 * n + 1, def), unit(def), N(n), func(f) {}
 
-  void update(int p, T val) {
-    for (seg[p += N] = val; p /= 2; ) {
-      seg[p] = func(seg[p * 2], seg[p * 2 + 1]);
-    }
-  }
+	void update(int p, T val) {
+		for (tree[p += N] = val; p /= 2; ) {
+			tree[p] = func(tree[p * 2], tree[p * 2 + 1]);
+		}
+	}
 
-  T query(int l, int r) {
-    T ra = unit, rb = unit;
-    for (l += N, r += N; l < r; r /= 2, l /= 2) {
-      if (l % 2) ra = func(ra, seg[l++]);
-      if (r % 2) rb = func(rb, seg[--r]);
-    }
-    return func(ra, rb);
-  }
+	T query(int l, int r) {
+		T ra = unit, rb = unit;
+		for (l += N, r += N; l < r; r /= 2, l /= 2) {
+			if (l % 2) ra = func(ra, tree[l++]);
+			if (r % 2) rb = func(rb, tree[--r]);
+		}
+		return func(ra, rb);
+	}
 
-  void build(vector<int> a) {
-    int n = int(a.size());
-    assert(N == n);
-    /*
-    for (int i = 0; i < n; ++i) {
-      update(i, a[i]);
-    }
-    */
-    for (int i = 0; i < n; ++i) {
-      seg[i + n] = a[i];
-    }
-    for (int i = n - 1; i >= 0; --i) {
-      seg[i] = func(seg[2 * i], seg[2 * i + 1]);
-    }
-  }
+	void build(const std::vector<int>& a) {
+		int n = int(a.size());
+		assert(N == n);
+		/*
+		for (int i = 0; i < n; ++i) {
+			update(i, a[i]);
+		}
+		*/
+		for (int i = 0; i < n; ++i) {
+			tree[i + n] = a[i];
+		}
+		for (int i = n - 1; i >= 0; --i) {
+			tree[i] = func(tree[2 * i], tree[2 * i + 1]);
+		}
+	}
 };
